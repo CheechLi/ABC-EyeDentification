e586ebc4772add32caa2f646df1e1222b1616485
Cheech Li <yingqili280@gmail.com>
2025-08-02 19:05:31 -0400

    Pages

---
+++ pages/Team.py
@@ -0,0 +1,38 @@
+import streamlit as st
+
+st.title("Meet the Team")
+
+st.markdown("""
+We are a student team from the **MIT Beaver Works Summer Institute (BWSI) Medlytics program**.  
+Our project focuses on using AI for early detection of eye diseases using retinal images.
+
+### Our Goal
+Our goal? Build a smart, accessible, and slightly nerdy tool that helps people learn about eye diseases—and maybe even spot them—just from fundus images (aka fancy eyeball pics). We want to raise awareness about how common and sneaky ocular conditions can be, and create a model that’s not here to replace your ophthalmologist, but to give them a helpful nudge. As we gather more data (thanks to generous volunteers and curious students), we’ll keep training and improving our model until it’s sharp enough to catch even the tiniest red flags—because healthy eyes deserve 20/20 tech.
+
+-----
+
+### Team Members
+- **Aaditya Penmetsa** –[role] Rising senior from Irvington High School in California. Interested in biomedical sciences with a strong passion for ophthalmology and AI in healthcare.
+
+- **Brigitta Yu** –[Website Designer] I’m passionate about biomedical and applied math. My work explores intersections between eye care, reproductive health, and kinesiology—using code and research to solve real-world problems.
+
+- **Cheech Li** – [Role] [intro]
+
+### Mentor
+- **Omar Mansour**
+
+
+
+### GitHub
+            
+https://github.com/CheechLi/Medlytics-Capstone-Website.git
+            
+### Contact Us
+
+aadipenmetsa@gmail.com
+
+chenluyu06@gmail.com
+
+yingqili280@gmail.com
+
+""")
