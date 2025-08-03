daeec93cabd875566179de2c607fc5ce7b0c8955
Cheech Li <yingqili280@gmail.com>
2025-08-02 20:01:32 -0400

    Team

--- pages/Team.py
+++ pages/Team.py
@@ -4,22 +4,41 @@ st.title("Meet the Team")
 
 st.markdown("""
 We are a student team from the **MIT Beaver Works Summer Institute (BWSI) Medlytics program**.  
-Our project focuses on using AI for early detection of eye diseases using retinal images.
+Our project focuses on using AI for early detection of ocular diseases using retinal images.
 
 ### Our Goal
-Our goal? Build a smart, accessible, and slightly nerdy tool that helps people learn about eye diseases—and maybe even spot them—just from fundus images (aka fancy eyeball pics). We want to raise awareness about how common and sneaky ocular conditions can be, and create a model that’s not here to replace your ophthalmologist, but to give them a helpful nudge. As we gather more data (thanks to generous volunteers and curious students), we’ll keep training and improving our model until it’s sharp enough to catch even the tiniest red flags—because healthy eyes deserve 20/20 tech.
-
+According to the World Health Organization (WHO), approximately 
+2.2 billion people worldwide suffer from vision impairment, yet many do not have access to regular eye care.
+At least a billion of these cases could have been prevented or treated with early detection and intervention.  
+We aim to bridge this gap by developing a smart classifier that can help detect common ocular diseases early
+and provide a platform for students and researchers to explore medical AI hands-on.
+We invite volunteers to upload ocular images to help us diversify our dataset and improve model performance.
+            
 -----
 
 ### Team Members
-- **Aaditya Penmetsa** –[role] Rising senior from Irvington High School in California. Interested in biomedical sciences with a strong passion for ophthalmology and AI in healthcare.
+- **Aaditya Penmetsa** –[Machine Learning Engineer] 
+            
+Rising senior from Irvington High School in California. Interested in biomedical sciences with a strong passion for ophthalmology and AI in healthcare.
 
-- **Brigitta Yu** –[Website Designer] I’m passionate about biomedical and applied math. My work explores intersections between eye care, reproductive health, and kinesiology—using code and research to solve real-world problems.
+- **Brigitta Yu** –[Web Designer] 
+            
+I’m passionate about biomedical and applied math. My work explores intersections between eye care, reproductive health, and kinesiology—using code and research to solve real-world problems.
 
-- **Cheech Li** – [Role] [intro]
+- **Yingqi (Cheech) Li** – [Product Manager & Machine Learning Engineer] 
+            
+Rising senior at Fort Lee High School in New Jersey. Cheech has served as his school's class president for the past three academic years. He is interested in the application and ethics of AI/ML in healthcare.
 
-### Mentor
-- **Omar Mansour**
+### Lead Instructor
+- **Omar Mansour** - Undergraduate student @ Columbia University
+            
+### Teaching Assistants
+            
+- **Carissma Mcgee** - PHD candidate @ Massachusetts Institute of Technology
+- **Pitipat Kongsomjit** - Undergraduate student @ Worcester Polytechnic Institute
+- **Andrea Valentino** - Undergraduate student @ Emory University
+- **Jayashabari Shankar** - Undergraduate student @ Massachusetts Institute of Technology
+- **Andrea Chen** - Undergraduate student @ University of Washington
 
 
 
@@ -29,10 +48,10 @@ https://github.com/CheechLi/Medlytics-Capstone-Website.git
             
 ### Contact Us
 
-aadipenmetsa@gmail.com
+Aaditya Penmetsa - aadipenmetsa@gmail.com
 
-chenluyu06@gmail.com
+Bridgitta Yu - chenluyu06@gmail.com
 
-yingqili280@gmail.com
+Yingqi (Cheech) Li - yingqili280@gmail.com
 
 """)
