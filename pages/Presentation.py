6db1e779a9742d271d6b799e0261e8102714ce5d
Cheech Li <yingqili280@gmail.com>
2025-08-03 00:12:12 -0400

    Video

---
+++ pages/Presentation.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.markdown("<h1 style='text-align: center;'>In-Depth Video Presentation</h1>", unsafe_allow_html=True)
+
+youtube_url = "https://youtu.be/z2hk7lZia24"  # Replace with your YouTube video URL
+st.video(youtube_url)
+
+st.markdown("""
+Voices Credits:
+1. Yingqi (Cheech) Li
+2. Aaditya Penmetsa
+3. Brigitta Yu
+4. Aaditya Penmetsa, Brigitta Yu, Yingqi (Cheech) Li
+5. Yingqi (Cheech) Li
+6. Aaditya Penmetsa, Brigitta Yu
+7. Yingqi (Cheech) Li
+
+""")
